ac92c5c9ba7b21158fb75976db719497421b3c57
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-01-12 08:36:01 +0100

    Ajoutez des fichiers projet.

---
+++ OBJ-OS/OBJ-OS.cpp
@@ -0,0 +1,27 @@
+﻿// OBJ-OS.cpp : définit le point d'entrée de l'application.
+//
+
+#include "OBJ-OS.h"
+
+using namespace std;
+
+int main()
+{
+	COS os;
+	if (os.getLinux() == true && (os.getWindows() == false))
+	{
+		cout << "LINUX" << endl;
+	}
+	else
+	{
+		if ((os.getLinux() == false) && (os.getWindows() == true))
+		{
+			cout << "WINDOWS" << endl;
+		}
+		else
+		{
+			cout << "AUTRE" << endl;
+		}
+	}
+	return 0;
+}
---
+++ OBJ-OS/OBJ-OS.h
@@ -0,0 +1,49 @@
+﻿// OBJ-OS.h : fichier Include pour les fichiers Include système standard,
+// ou les fichiers Include spécifiques aux projets.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
+class COS
+{
+private :
+	bool ewindows;
+	bool elinux;
+public :
+		COS();
+		~COS();
+		bool getWindows();
+		bool getLinux();
+};
+
+
+COS::COS()
+{
+#ifdef __linux__
+	elinux = true;
+	ewindows = false;
+#elif _WIN32
+	elinux = false;
+	ewindows = true;
+#else
+	elinux = false;
+	ewindows = false;
+#endif
+}
+
+COS::~COS()
+{
+
+}
+
+bool COS::getLinux()
+{
+	return elinux;
+}
+
+bool  COS::getWindows()
+{
+	return ewindows;
+}
